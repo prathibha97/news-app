601c4d3e1c35ae0d417d024f68ff8e9412953544
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-08 15:45:49 +0530

    Add SearchBox component

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
 import Link from 'next/link';
 import NavLinks from './NavLinks';
+import SearchBox from './SearchBox';
 const Header = () => {
   return (
     <header>
@@ -23,9 +24,8 @@ const Header = () => {
           </button>
         </div>
       </div>
-      <NavLinks/>
-
-      {/* searchbox */}
+      <NavLinks />
+      <SearchBox />
     </header>
   );
 };
