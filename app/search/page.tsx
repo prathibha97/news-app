315963da8a518d3837546399a82487f260b98102
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-09 11:49:55 +0530

    Add Search page

---
+++ app/search/page.tsx
@@ -0,0 +1,22 @@
+import fetchNews from '../../lib/fetchNews';
+import NewsList from '../NewsList';
+
+interface IProps {
+  searchParams?: { term: string };
+}
+
+const SearchPage = async ({ searchParams }: IProps) => {
+  const news: NewsResponse = await fetchNews(
+    'general',
+    searchParams?.term,
+    true
+  );
+  return (
+    <div>
+      <h1 className="headerTitle">Search Results for: {searchParams?.term}</h1>
+      <NewsList news={news} />
+    </div>
+  );
+};
+
+export default SearchPage;
