742b56df22d72874994b4191ac6f10de44f35434
Prathibha Ratnayake <prsthibha@gmail.com>
2022-12-09 12:24:24 +0530

    Add loading component

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+  return (
+    <div className='p-10 font-serif text-lg text-center text-gray-400 animate-pulse'>
+      Loading News Feed...
+      </div>
+  )
+}
+
+export default Loading
